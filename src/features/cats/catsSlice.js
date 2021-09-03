3e7dff8c01a4ba36211451f494ebd456e859d1f1
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-09-04 00:08:06 +0300

    update

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,34 +1,15 @@
-// Action Creators
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 // async actions
-export function fetchCats() {
-  return function (dispatch) {
-    dispatch({ type: "cats/fetchCats/pending" });
-    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: "cats/fetchCats/fulfilled",
-          payload: data.images,
-        });
-      });
-  };
-}
+export const fetchCats = createAsyncThunk("cats/fetchCats", async () => {
+  const request = await fetch(
+    "https://learn-co-curriculum.github.io/cat-api/cats.json"
+  );
 
-// sync actions added for demo purposes
-export function catAdded(newCat) {
-  return {
-    type: "cats/catAdded",
-    payload: newCat,
-  };
-}
+  const data = await request.json();
 
-export function catUpdated(updatedCat) {
-  return {
-    type: "cats/catUpdated",
-    payload: updatedCat,
-  };
-}
+  return data.images;
+});
 
 // Reducer
 const initialState = {
@@ -36,38 +17,37 @@ const initialState = {
   status: "idle", // loading state
 };
 
-function catsReducer(state = initialState, action) {
-  switch (action.type) {
-    // sync actions
-    case "cats/catAdded":
-      return {
-        ...state,
-        entities: [...state.entities, action.payload],
-      };
-    case "cats/catUpdated":
-      return {
-        ...state,
-        entities: state.entities.map((cat) =>
-          cat.id === action.payload.id ? action.payload : cat
-        ),
-      };
+const catsSlice = createSlice({
+  initialState: initialState,
+  name: "cats",
+  reducers: {
+    catAdded(state, action) {
+      state.entities.push(action.payload);
+    },
 
-    // async actions
-    case "cats/fetchCats/pending":
-      return {
-        ...state,
-        status: "loading",
-      };
-    case "cats/fetchCats/fulfilled":
-      return {
-        ...state,
-        entities: action.payload,
-        status: "idle",
-      };
+    catRemoved(state, action) {
+      state.entities = state.entities.filter(
+        (cat) => cat.id !== action.payload
+      );
+    },
 
-    default:
-      return state;
-  }
-}
+    catUpdated(state, action) {
+      const cat = state.entities.find((cat) => cat.id === action.payload.id);
+      cat.url = action.payload.url;
+    },
+  },
+  extraReducers: {
+    [fetchCats.pending](state) {
+      state.status = "loading";
+    },
 
-export default catsReducer;
+    [fetchCats.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
+    },
+  },
+});
+
+export const { catUpdated, catRemoved, catAdded } = catsSlice.actions;
+
+export default catsSlice.reducer;
